91a9cdc8ce5edee15ec08d2ae7a27c9b92d91f14
Harjot <singh.harjot85@gmail.com>
2019-03-15 14:24:31 -0700

    Modify project structure

---
+++ src/pages/projects/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import React, { Component } from 'react'
+
+export default class Projects extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
