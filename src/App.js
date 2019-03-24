569c7b78e22697b6e940282cfbd7cb50a11e2282
Harjot <singh.harjot85@gmail.com>
2019-03-24 16:42:18 -0700

    Render Footer in Home instead of in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home/home";
-import Footer from "./components/footer/footer";
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Route path="/" component={Home} />
-        <Route path="/footer" component={Footer} />
       </Router>
     );
   }
