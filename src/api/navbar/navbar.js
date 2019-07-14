4a57185430de105e56936181a2ab9bcf246d3f7d
Harjot <singh.harjot85@gmail.com>
2019-07-14 16:24:21 -0700

    Fix api url

--- src/api/navbar/navbar.js
+++ src/api/navbar/navbar.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export function getNavbarItems() {
   return axios.get(
-    // "https://staging-api-chipinomeshworks.herokuapp.com/api/navbar",
-    "https://localhost:44302/api/navbar",
+    "https://staging-api-chipinomeshworks.herokuapp.com/api/navbar",
     {
       method: "GET"
     }
