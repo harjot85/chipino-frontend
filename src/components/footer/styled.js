b3da7b295a58185d9884e1f7d1296c42cfc3cdc0
Harjot <singh.harjot85@gmail.com>
2019-07-24 19:52:01 -0700

    footer tries

--- src/components/footer/styled.js
+++ src/components/footer/styled.js
@@ -8,14 +8,22 @@ export const FooterStyled = styled(Col)`
   bottom: 0;
   text-align: center;
   margin-top: 4%;
+  position: fixed;
 `;
 
 export const FooterIcon = styled.div`
-  font-size: 48px;
-  color: white;
-  padding: 6% 1% 6% 1%;
-  
+  font-size: 64px;
+  color: grey;
+  padding: 8px;
+  margin: 6% 10px;
+  border-radius: 50%;
+  transition: all .2s ease-in-out; 
+
   :hover {
     color: ${props => props.hoverColor || "blue"};
+    transform: scale(1.1); 
+    border-bottom-color: ${props => props.hoverColor || "blue"};
+    box-shadow:
+    0 0 30px 10px ${props => props.hoverColor || "blue"};
   }
 `;
